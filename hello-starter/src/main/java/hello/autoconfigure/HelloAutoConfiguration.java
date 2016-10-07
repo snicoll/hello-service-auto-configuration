2397876653173cd489853e43de371556ba70b1ce
Stephane Nicoll <snicoll@pivotal.io>
2016-10-07 13:24:36 +0200

    Basic auto-configuration
    This creates a very basic (dumb) auto-configuration. It kicks in
    unconditionally and erases our own bean definition. This is certainly not
    what we want to do. We'll improve the auto-configuration in the next
    commit.
    
    This showcases that an auto-configuration is a plain configuration that
    is registered in `META-INF/spring.factories`.

--- hello-starter/src/main/java/hello/autoconfigure/HelloAutoConfiguration.java
+++ hello-starter/src/main/java/hello/autoconfigure/HelloAutoConfiguration.java
@@ -1,4 +1,17 @@
 package hello.autoconfigure;
 
+import hello.ConsoleHelloService;
+import hello.HelloService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class HelloAutoConfiguration {
+
+	@Bean
+	public HelloService helloService() {
+		return new ConsoleHelloService();
+	}
+
 }
--- hello-starter/src/test/java/hello/autoconfigure/HelloAutoConfigurationTest.java
+++ hello-starter/src/test/java/hello/autoconfigure/HelloAutoConfigurationTest.java
@@ -1,11 +1,14 @@
 package hello.autoconfigure;
 
+import hello.HelloService;
 import org.junit.Rule;
 import org.junit.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.rule.OutputCapture;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloAutoConfigurationTest {
 
@@ -17,7 +20,12 @@ public class HelloAutoConfigurationTest {
 
 	@Test
 	public void defaultServiceIsAutoConfigured() {
-
+		this.contextRunner.run((context) -> {
+			assertThat(context).hasSingleBean(HelloService.class);
+			HelloService bean = context.getBean(HelloService.class);
+			bean.sayHello("World");
+			assertThat(this.output.toString()).contains("Hello World!");
+		});
 	}
 
 }
